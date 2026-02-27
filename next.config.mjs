2904d89f03ab0ac5fe2d2f21831a365ad64b0d3e
SimoG3 <192517110+SimoG3@users.noreply.github.com>
2026-02-27 23:28:39 +0000

    fixed export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   productionBrowserSourceMaps: true,
   distDir: process.env.DIST_DIR || '.next',  typescript: {
     ignoreBuildErrors: true,
