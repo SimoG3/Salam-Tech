d7c9e3b9dfb7e00900dede4651e81e6b0ae4da6c
SimoG3 <192517110+SimoG3@users.noreply.github.com>
2026-03-03 17:18:05 +0000

    removed rocket logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,7 @@ export default function RootLayout({
         </Suspense>
         {children}
 
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Ftechboutiq1571back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.16" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+      </body>
     </html>
   );
 }
